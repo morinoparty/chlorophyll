ba5cbd25c1355439eab711ab04667df96557dea0
nlkomaru <nikomaru@nikomaru.dev>
2025-05-20 12:54:59 +0900

    :sparkles: Fix ci

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,10 +5,8 @@ const config: StorybookConfig = {
     addons: [
         "@storybook/addon-essentials",
         "@storybook/addon-onboarding",
-        "@chromatic-com/storybook",
         "@storybook/experimental-addon-test",
         "@storybook/addon-a11y",
-        "storycap",
         // "storybook-design-token",
     ],
     framework: {
